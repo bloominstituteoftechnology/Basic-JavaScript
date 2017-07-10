87d9edd1a75877660a8cdea62afaacb7ad166f9b
Gregory Knudsen <dev.artist15@gmail.com>
2017-07-10 19:52:04 -0400

    test 6 passes

--- src/project-2.js
+++ src/project-2.js
@@ -61,6 +61,13 @@ const fizzBuzz = (num) => {
   // if num is divisible by 5 return 'buzz'
   // if num is divisible by 3 & 5 return 'fizzbuzz'
   // otherwise return num
+  if (num % 3 === 0 && num % 5 === 0) {
+    return 'fizzbuzz';
+  } else if (num % 3 === 0) {
+    return 'fizz';
+  } else if (num % 5 === 0) {
+    return 'buzz';
+  } return num;
 };
 
 const isPrime = (num) => {
