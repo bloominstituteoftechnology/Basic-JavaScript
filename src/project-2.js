5348a9be7e3f0c6251b0216e73521e0a18f83d36
Gregory Knudsen <dev.artist15@gmail.com>
2017-07-10 19:12:12 -0400

    test 1 passes

--- src/project-2.js
+++ src/project-2.js
@@ -3,6 +3,13 @@
 const getBiggest = (x, y) => {
   // x and y are integers.  Return the larger integer
   // if they are the same return either one
+  if (x === y) {
+    return x || y;
+  } else if (x > y) {
+    return x;
+  } else if (y > x) {
+    return y;
+  }
 };
 
 const greeting = (language) => {
