036a450d80d6825e2f4675581d50490b1cf522e5
Calvin Le <calvin@Calvins-Air.fios-router.home>
2017-07-11 19:18:08 -0400

    updated hw

--- src/project-2.js
+++ src/project-2.js
@@ -1,11 +1,25 @@
 // Do not change any of the function names
 
 const getBiggest = (x, y) => {
+  if (x > y) {
+    return x;
+  } else if (x < y) {
+    return y;
+  }
+  return x || y;
   // x and y are integers.  Return the larger integer
   // if they are the same return either one
 };
 
 const greeting = (language) => {
+  if (language === 'German') {
+    return 'Guten Tag!';
+  } else if (language === 'English') {
+    return 'Hello!';
+  } else if (language === 'Spanish') {
+    return 'Hola!';
+  }
+  return 'Hello!';
   // return a greeting for three different languages:
   // language: 'German' -> 'Guten Tag!'
   // language: 'English' -> 'Hello!'
@@ -16,10 +30,18 @@ const greeting = (language) => {
 const isTenOrFive = (num) => {
   // return true if num is 10 or 5
   // otherwise return false
+  if (num === 10 || num === 5) {
+    return true;
+  }
+  return false;
 };
 
 const isInRange = (num) => {
   // return true if num is less than 50 and greater than 20
+  if (num < 50 && num > 20) {
+    return true;
+  }
+  return false;
 };
 
 const isInteger = (num) => {
@@ -36,6 +58,14 @@ const fizzBuzz = (num) => {
   // if num is divisible by 5 return 'buzz'
   // if num is divisible by 3 & 5 return 'fizzbuzz'
   // otherwise return num
+  if (num % 3 === 0) {
+    return 'fizz';
+  } else if (num % 5 === 0) {
+    return 'buzz';
+  } else if (num % 3 === 0 && num % 5 === 0) {
+    return 'fizzbuzz';
+  }
+  return num;
 };
 
 const isPrime = (num) => {
@@ -48,31 +78,39 @@ const isPrime = (num) => {
 
 const returnFirst = (arr) => {
   // return the first item from the array
+  return arr[0];
 };
 
 const returnLast = (arr) => {
   // return the last item of the array
+  return arr[arr.length - 1];
 };
 
 const getArrayLength = (arr) => {
   // return the length of the array
+  return arr.length;
 };
 
 const incrementByOne = (arr) => {
   // arr is an array of integers
   // increase each integer by one
   // return the array
+  for (let i = 0; i < arr.length; i++) {
+    return arr += 1;
+  }
 };
 
 const addItemToArray = (arr, item) => {
   // add the item to the end of the array
   // return the array
+  return arr.push[item];
 };
 
 const addItemToFront = (arr, item) => {
   // add the item to the front of the array
   // return the array
   // hint: use the array method .unshift
+  return arr.unshift[item];
 };
 
 const wordsToSentence = (words) => {
@@ -80,26 +118,50 @@ const wordsToSentence = (words) => {
   // return a string that is all of the words concatenated together
   // spaces need to be between each word
   // example: ['Hello', 'world!'] -> 'Hello world!'
+  return words.join();
 };
 
 const contains = (arr, item) => {
   // check to see if item is inside of arr
   // return true if it is, otherwise return false
+  for (let i = 0; i < arr.length; i++) {
+    if (item === arr[i]) {
+      return true;
+    }
+  }
+  return false;
 };
 
 const addNumbers = (numbers) => {
   // numbers is an array of integers.
   // add all of the integers and return the value
+  let total = 0;
+  for (let i = 0; i < numbers.length; i++) {
+    total += numbers[i];
+  }
+  return total;
 };
 
 const averageTestScore = (testScores) => {
   // testScores is an array.  Iterate over testScores and compute the average.
   // return the average
+  let total = 0;
+  for (let i = 0; i < testScores.length; i++) {
+    total += testScores[i];
+  }
+  return total / testScores.length;
 };
 
 const largestNumber = (numbers) => {
   // numbers is an array of integers
   // return the largest integer
+  let largest = 0;
+  for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] > largest) {
+      largest = numbers[i];
+    }
+  }
+  return largest;
 };
 
 // Do not modify code below this line.
--- src/project-4.js
+++ src/project-4.js
@@ -4,9 +4,11 @@ const multiplyArguments = () => {
   // use the arguments keyword to multiply all of the arguments together and return the product
   // if no arguments are passed in return 0
   // if one argument is passed in just return it
+
 };
 
 const invokeCallback = (cb) => {
+  return cb();
   // invoke cb
 };
 
