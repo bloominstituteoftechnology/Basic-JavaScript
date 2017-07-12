3b245bd5f46cca0f45834fae2cf41a6f26899316
sunjieming <ben@lambdaschool.com>
2017-07-11 21:00:34 -0700

    Update description

--- src/project-3.js
+++ src/project-3.js
@@ -78,13 +78,15 @@ const sumUserPostLikes = (user) => {
 };
 
 const addCalculateDiscountPriceMethod = (storeItem) => {
-  // add a method to the storeItem object called 'calculateDiscountPrice'
-  // this method should multiply the storeItem's 'price' and 'discountPercentage' to get the discount
-  // the method then subtracts the discount from the price and returns the discounted price
+  // Add a method to the storeItem object called 'calculateDiscountPrice'
+  // This method should multiply the storeItem's 'price' and 'discountPercentage' to get the discount
+  // The method then subtracts the discount from the price and returns the discounted price
   // example:
   // price -> 20
   // discountPercentage -> .2
   // discountPrice = 20 - (20 * .2)
+  // Make sure you return storeItem after adding the method to it
+  // hint: arrow functions don't bind a this
 };
 
 // Do not modify code below this line.
