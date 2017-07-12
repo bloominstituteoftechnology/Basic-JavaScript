6e69f300d748d8e0e0e0b52aa4dfbcc1f4877fca
Colombianito66 <colombianito66@gmail.com>
2017-07-12 03:08:19 -0400

    project-4 best attempt; 8 tests not passing

--- src/project-4.js
+++ src/project-4.js
@@ -1,4 +1,8 @@
 // Do not change any of the function names
+// QUESTIONS: multiplyArguments, getUserConstructor, cacheFunction;
+//  Could not figure out 'error  Parsing error: Unexpected token'
+// always on the last line of the program
+// which did not allow us to run the last several tests..
 
 const multiplyArguments = () => {
   // use the arguments keyword to multiply all of the arguments together and return the product
@@ -26,15 +30,27 @@ const multiplyArguments = () => {
   //   console.log(arguments[i]);
   // }
   // return total;
-  const args = [...arguments];
-  if (args.length === 0) {
-    return 0;
-  } else if (args.length === 1) {
-    return args[0];
-  }
-  return args.reduce((previous, arg) => {
-    return previous *= arg;
-  }, 1);
+
+  // const args = [...arguments];
+  // if (args.length === 0) {
+  //   return 0;
+  // } else if (args.length === 1) {
+  //   return args[0];
+  // }
+  // return args.reduce((previous, arg) => {
+  //   return previous *= arg;
+  // }, 1);
+  const argsProduct = (...args) => {
+    if (args.length === 0) {
+      return 0;
+    } else if (args.length === 1) {
+      return args[0];
+    }
+    let total = 1;
+    for (let i = 0; i < args.length; i++) {
+      total *= args[i];
+    }
+    return total;
 };
 
 const invokeCallback = (cb) => {
@@ -46,7 +62,7 @@ const sumArray = (numbers, cb) => {
   // sum up all of the integers in the numbers array
   // pass the result to cb
   // no return is necessary
-  let total;
+  let total = 0;
   for (let i = 0; i < numbers.length; i++) {
     total += numbers[i];
   }
@@ -56,12 +72,19 @@ const sumArray = (numbers, cb) => {
 const forEach = (arr, cb) => {
   // iterate over arr and pass its values to cb one by one
   // hint: you will be invoking cb multiple times (once for each value in the array)
+  arr.forEach((num) => {
+    cb(num);
+  });
 };
 
 const map = (arr, cb) => {
   // create a new array
   // iterate over each value in arr, pass it to cb, then place the value returned from cb into the new arr
   // the new array should be the same length as the array argument
+  const newArray = arr.map((index) => {
+    return cb(index);
+  });
+  return newArray;
 };
 
 const getUserConstructor = () => {
@@ -71,11 +94,26 @@ const getUserConstructor = () => {
   // the constructor should have a method 'sayHi' on its prototype that returns the string 'Hello, my name is {{name}}'
   // {{name}} should be the name set on each instance
   // return the constructor
+  //
+  // FIRST ATTEMPT
+  const User = (options) => {
+    this.username = options.username,
+    this.name = options.name,
+    this.email = options.email,
+    this.password = options.password
+   };
+    User.prototype.sayHi = () => {
+      return `Hello, my name is ${this.name}`;
+    };
+  return User;
 };
 
 const addPrototypeMethod = (Constructor) => {
   // add a method to the constructor's prototype
   // the method should be called 'sayHi' and should return the string 'Hello World!'
+  Constructor.prototype.sayHi = () => {
+    return 'Hello World!';
+  }
 };
 
 const addReverseString = () => {
@@ -83,6 +121,9 @@ const addReverseString = () => {
   // name this method reverse
   // hint:
   // you will need to use 'this' inside of reverse
+  String.prototype.reverse = (string) => {
+    this.string.split().reverse().join();
+  }
 };
 
 const nFactorial = (n) => {
@@ -90,6 +131,9 @@ const nFactorial = (n) => {
   // solve this recursively
   // example:
   // the factorial of 3 is 6 (3 * 2 * 1)
+  if (n < 2) {
+    return 1;
+  } return n * nFactorial(n - 1);
 };
 
 const cacheFunction = (cb) => {
@@ -105,7 +149,6 @@ const cacheFunction = (cb) => {
   // if the function you return is invoked again with 5 it will look on an object in the closure scope
   // and return 25 directly and will not invoke cb again
 };
-
 
 // Do not modify code below this line.
 // --------------------------------
