8e5e495d6bc841dbf98170b199f3cf190679e162
Sandeep Chandran <deepsun80@yahoo.com>
2017-07-12 21:54:55 -0500

    Project 4 Updated

--- src/project-4.js
+++ src/project-4.js
@@ -82,6 +82,9 @@ const addReverseString = () => {
   // name this method reverse
   // hint:
   // you will need to use 'this' inside of reverse
+  String.prototype.reverse = function () {
+    return this.split('').reverse().join('');
+  };
 };
 
 const nFactorial = (n) => {
