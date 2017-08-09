52c65d2e79034e1c4a52c6e818615a3b44cc1066
Ryan Hamblin <ryanhamblin@Ryans-MBP.domain>
2017-08-09 07:39:29 -0600

    added solutions

--- src/project-2.js
+++ src/project-2.js
@@ -1,109 +1,153 @@
-// Do not change any of the function names
-
 const getBiggest = (x, y) => {
-  // x and y are integers.  Return the larger integer
-  // if they are the same return either one
+  if (x > y) {
+    return x;
+  } else if (x === y) {
+    return y;
+  }
+  return y;
 };
 
 const greeting = (language) => {
-  // return a greeting for three different languages:
-  // language: 'German' -> 'Guten Tag!'
-  // language: 'Spanish' -> 'Hola!'
-  // language: 'Chinese' -> 'Ni Hao!'
-  // if language is undefined return 'Hello!'
+  switch (language) {
+    case 'German':
+      return 'Guten Tag!';
+    case 'Spanish':
+      return 'Hola!';
+    case 'Chinese':
+      return 'Ni Hao!';
+    default:
+      return 'Hello!';
+  }
 };
 
 const isTenOrFive = (num) => {
-  // return true if num is 10 or 5
-  // otherwise return false
+  if (num === 10 || num === 5) {
+    return true;
+  }
+  return false;
 };
 
 const isInRange = (num) => {
-  // return true if num is less than 50 and greater than 20
+  if (num < 50 && num > 20) {
+    return true;
+  }
+  return false;
 };
 
 const isInteger = (num) => {
-  // return true if num is an integer
-  // 0.8 -> false
-  // 1 -> true
-  // -10 -> true
-  // otherwise return false
-  // hint: you can solve this using Math.floor
+  if (Math.floor(num) === num) {
+    return true;
+  }
+  return false;
 };
 
 const fizzBuzz = (num) => {
-  // if num is divisible by 3 return 'fizz'
-  // if num is divisible by 5 return 'buzz'
-  // if num is divisible by 3 & 5 return 'fizzbuzz'
-  // otherwise return num
+  if (num % 5 === 0 && num % 3 === 0) {
+    return 'fizzbuzz';
+  } else if (num % 5 === 0) {
+    return 'buzz';
+  } else if (num % 3 === 0) {
+    return 'fizz';
+  }
+  return num;
 };
 
 const isPrime = (num) => {
-  // return true if num is prime.
-  // otherwise return false
-  // hint: a prime number is only evenly divisible by itself and 1
-  // hint2: you can solve this using a for loop
-  // note: 0 and 1 are NOT considered prime numbers
+  if (num < 0) {
+    return false;
+  }
+  if (num === 1 || num === 0) {
+    return false;
+  }
+  for (let i = 2; i < num; i++) {
+    if (num % i === 0) {
+      return false;
+    }
+  }
+  return true;
 };
 
 const returnFirst = (arr) => {
-  // return the first item from the array
+  return arr[0];
 };
 
 const returnLast = (arr) => {
-  // return the last item of the array
+  return arr[arr.length - 1];
 };
 
 const getArrayLength = (arr) => {
-  // return the length of the array
+  return arr.length;
 };
 
 const incrementByOne = (arr) => {
-  // arr is an array of integers
-  // increase each integer by one
-  // return the array
+  for (let i = 0; i < arr.length; i++) {
+    arr[i]++;
+  }
+  return arr;
 };
 
 const addItemToArray = (arr, item) => {
-  // add the item to the end of the array
-  // return the array
+  arr.push(item);
+  return arr;
 };
 
 const addItemToFront = (arr, item) => {
-  // add the item to the front of the array
-  // return the array
-  // hint: use the array method .unshift
+  arr.unshift(item);
+  return arr;
 };
 
 const wordsToSentence = (words) => {
-  // words is an array of strings
-  // return a string that is all of the words concatenated together
-  // spaces need to be between each word
-  // example: ['Hello', 'world!'] -> 'Hello world!'
+  let newSentence = '';
+  for (let i = 0; i < words.length; i++) {
+    if (i === 0) {
+      newSentence += `${words[i]}`;
+    } else {
+      newSentence += ` ${words[i]}`;
+    }
+  }
+  return newSentence;
 };
 
 const contains = (arr, item) => {
-  // check to see if item is inside of arr
-  // return true if it is, otherwise return false
+  let itemCounter = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === item) {
+      itemCounter++;
+    }
+  }
+  if (itemCounter > 0) {
+    return true;
+  }
+  return false;
 };
 
 const addNumbers = (numbers) => {
-  // numbers is an array of integers.
-  // add all of the integers and return the value
+  let sumOfNumbers = 0;
+  for (let i = 0; i < numbers.length; i++) {
+    sumOfNumbers += numbers[i];
+  }
+  return sumOfNumbers;
 };
 
 const averageTestScore = (testScores) => {
-  // testScores is an array.  Iterate over testScores and compute the average.
-  // return the average
+  let totalSumScores = 0;
+  let numberOfScore = 0;
+  for (let i = 0; i < testScores.length; i++) {
+    totalSumScores += testScores[i];
+    numberOfScore++;
+  }
+  return totalSumScores / numberOfScore;
 };
 
 const largestNumber = (numbers) => {
-  // numbers is an array of integers
-  // return the largest integer
+  let biggestInteger = 0;
+  for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] > biggestInteger) {
+      biggestInteger = numbers[i];
+    }
+  }
+  return biggestInteger;
 };
-
-// Do not modify code below this line.
-// --------------------------------
 
 module.exports = {
   getBiggest,
--- src/project-4.js
+++ src/project-4.js
@@ -1,31 +1,37 @@
 const getFirstItem = (collection, cb) => {
-  // invoke the callback function and pass the first item from the collection in as an argument
+  cb(collection[0]);
 };
 
 const getLength = (collection, cb) => {
-  // Write a function called getLength that passes the length of the array into the callback
+  cb(collection.length);
 };
 
 const getLastItem = (collection, cb) => {
-  // Write a function called getLastItem which passes the getLastItem item of the array into the callback
+  cb(collection[collection.length - 1]);
 };
 
 const sumNums = (x, y, cb) => {
-  // Write a function called sumNums that adds two numbers and passes the result to the callback
+  const numSummed = x + y;
+  cb(numSummed);
 };
 
 const multiplyNums = (x, y, cb) => {
-  // Write a function called multiplyNums that adds two numbers and passes the result to the callback
+  const numProduct = x * y;
+  cb(numProduct);
 };
 
 const contains = (collection, item, cb) => {
-  // Write a function called contains that checks if an item is present inside of the given array.
-  // Pass true to the callback if it is, otherwise pass false
+  cb(collection.includes(item));
 };
 
 const removeDuplicates = (collection, cb) => {
-  // Write a function called removeDuplicates that removes all duplicate values from the given array.
-  // Pass the array to the callback function.  Do not mutate the original array.
+  const itemsChecked = [];
+  collection.forEach((item, index) => {
+    if (itemsChecked.indexOf(item) === -1) {
+      itemsChecked.push(item);
+    }
+  });
+  cb(itemsChecked);
 };
 
 module.exports = {
