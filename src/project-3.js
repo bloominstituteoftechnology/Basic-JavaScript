d7141a40365c59e409e2a1cdf37715da2444f216
EricB <ericblancas23@gmail.com>
2017-08-08 18:08:52 -0700

    hw

--- src/project-3.js
+++ src/project-3.js
@@ -8,7 +8,7 @@ const makeCat = (name, age) => {
   const cat = {
     name: "Spike",
     age: 10,
-    const meow: () => {
+   meow: () => {
       console.log("Meow!");
     }
   }
@@ -86,46 +86,118 @@ const newUser = (name, email, password) => {
 
 const hasEmail = (user) => {
   // return true if the user has a value for the property 'email'
-  // otherwise return false
+  // otherwise return false 
+  const mail = {
+    email: user,
+  }
+  if (typeof mail.email === "undefined") {
+    return false;
+  } else {
+    return true; 
+  }
 };
-
+  const check = hasEmail(); 
+  console.log(check); 
+//do later
 const hasProperty = (object, property) => {
   // return true if the object has the value of the property argument
   // property is a string
-  // otherwise return false
+  // otherwise return false 
+  const pet = {
+    dog: property, 
+    fish: property,
+  } 
+  if (pet.fish === "goldfish" || pet.dog === "poodle") {
+    return true;
+  } else { 
+    return false;
+  }
 };
+  const animal = hasProperty("goldfish"); 
+  console.log(animal);
 
 const verifyPassword = (user, password) => {
   // check to see if the provided password matches the password property on the user object
   // return true if they match
   // otherwise return false
+  const user1 = {
+    name: user,
+    pin: '123',
+  }
+  if (user1.pin === password) {
+    return true;
+  } else {
+    return false;
+  }
 };
+  const pass = verifyPassword('eric', '1234'); 
+  console.log(pass); 
 
 const updatePassword = (user, newPassword) => {
   // replace the existing password on the user object with the value of newPassword
-  // return the object
+  // return the object 
+  const user2 = {
+    name: user,
+    password: '123',
+  } 
+  user2.password = newPassword;
+  return user2
 };
+  const newUser2 = updatePassword('eric', '12345');
+  console.log(newUser2);
 
 const addFriend = (user, newFriend) => {
   // user has a property called friends that is an array
   // add newFriend to the end of the friends array
-  // return the user object
+  // return the user object 
+  const pers = {
+    name: user,
+    friends: ['asd', 'gvh', 'hjk'],
+  }
+  pers.friends.splice(2, 0, newFriend); 
+  return pers;
 };
+  const human = addFriend('eric', 'allan'); 
+  console.log(human); 
 
 const setUsersToPremium = (users) => {
   // users is an array of user objects.
   // each user object has the property 'isPremium'
   // set each user's isPremium property to true
-  // return the users array
+  // return the users array 
+  const people = [{
+    name: users, 
+    isPremium: true
+  }, 
+] 
+return people;
 };
+  const peple = setUsersToPremium('eric');
+  console.log(peple);
+  
+const users = {
+  posts: [{
+    likes:11,
+    likes:12,
+    likes:14,
+  }]
+}
 
 const sumUserPostLikes = (user) => {
   // user has an array property called 'posts'
   // posts is an array of post objects
   // each post object has an integer property called 'likes'
   // sum together the likes from all the post objects
-  // return the sum
-};
+  // return the sum 
+  let sum = 0;
+  for (let i = 0; i < user.posts.length; i++) {
+    sum += user.posts[i].likes;
+   }
+   return sum;
+}
+ 
+const sumUser = sumUserPostLikes(); 
+console.log(sumUser);
 
 const addCalculateDiscountPriceMethod = (storeItem) => {
   // Add a method to the storeItem object called 'calculateDiscountPrice'
