6887ef81d54741bafc0d608a928af46e4465e5d1
Darryl Green <darryl.alexander.green@gmail.com>
2017-09-05 18:12:53 -0500

    All projects updated

--- src/project-4.js
+++ src/project-4.js
@@ -1,29 +1,48 @@
 const getFirstItem = (collection, cb) => {
+  cb(collection[0]);
   // invoke the callback function and pass the first item from the collection in as an argument
 };
 
 const getLength = (collection, cb) => {
+  cb(collection.length);
   // Write a function called getLength that passes the length of the array into the callback
 };
 
 const getLastItem = (collection, cb) => {
+  cb(collection[collection.length - 1]);
   // Write a function called getLastItem which passes the getLastItem item of the array into the callback
 };
 
 const sumNums = (x, y, cb) => {
+  cb(x + y);
   // Write a function called sumNums that adds two numbers and passes the result to the callback
 };
 
 const multiplyNums = (x, y, cb) => {
+  cb(x * y);
   // Write a function called multiplyNums that multiplies two numbers and passes the result to the callback
 };
 
 const contains = (collection, item, cb) => {
+  let isPresent = false;
+  for (let i = 0; i < collection.length; i++) {
+    if (collection[i] === item) {
+      isPresent = true;
+    }
+  } cb(isPresent);
   // Write a function called contains that checks if an item is present inside of the given array.
   // Pass true to the callback if it is, otherwise pass false
 };
 
 const removeDuplicates = (collection, cb) => {
+  const newArray = collection.slice();
+  for (let i = 0; i < newArray.length; i++) {
+    for (let j = i + 1; j < newArray.length; j++) {
+      if (newArray[i] === newArray[j]) {
+        newArray.splice(j, 1);
+      }
+    }
+  } cb(newArray);
   // Write a function called removeDuplicates that removes all duplicate values from the given array.
   // Pass the array to the callback function.  Do not mutate the original array.
 };
