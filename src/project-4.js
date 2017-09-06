cbfd97ed32dcf2ac124d27ec5b47768b6ee99d24
Brandon Fizer <30492636+Track7Dev@users.noreply.github.com>
2017-09-06 12:53:49 -0400

    Project 4: In-Progress

--- src/project-4.js
+++ src/project-4.js
@@ -1,27 +1,26 @@
-const getFirstItem = (collection, cb) => {
-  // invoke the callback function and pass the first item from the collection in as an argument
-};
-
-const getLength = (collection, cb) => {
+const getFirstItem = (collection, cb) => cb(collection[0]);
+ // invoke the callback function and pass the first item from the collection in as an argument
+const getLength = (collection, cb) => cb(collection.length);
   // Write a function called getLength that passes the length of the array into the callback
-};
-
-const getLastItem = (collection, cb) => {
+const getLastItem = (collection, cb) => cb(collection[collection.length - 1]);
   // Write a function called getLastItem which passes the getLastItem item of the array into the callback
-};
 
-const sumNums = (x, y, cb) => {
+const sumNums = (x, y, cb) => cb(x + y);
   // Write a function called sumNums that adds two numbers and passes the result to the callback
-};
 
-const multiplyNums = (x, y, cb) => {
+const multiplyNums = (x, y, cb) => cb(x * y);
   // Write a function called multiplyNums that multiplies two numbers and passes the result to the callback
-};
+
 
 const contains = (collection, item, cb) => {
+  for (let i = 0; i < collection.length; i++) {
+    if (collection[i].val === item.val) cb(true);
+    cb(false);
+  }
+};
   // Write a function called contains that checks if an item is present inside of the given array.
   // Pass true to the callback if it is, otherwise pass false
-};
+
 
 const removeDuplicates = (collection, cb) => {
   // Write a function called removeDuplicates that removes all duplicate values from the given array.
