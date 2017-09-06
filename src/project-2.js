9d2cbbcb32f4e8fc4ca3e5df62ff8b3f98d70402
Neil Barduson <barduson.neil@gmail.com>
2017-09-05 22:16:48 -0500

    updated pre-course work to ES6 syntax

--- src/project-2.js
+++ src/project-2.js
@@ -3,6 +3,10 @@
 const getBiggest = (x, y) => {
   // x and y are integers.  Return the larger integer
   // if they are the same return either one
+  if (x > y) {
+    return x;
+  }
+  return y;
 };
 
 const greeting = (language) => {
@@ -11,15 +15,21 @@ const greeting = (language) => {
   // language: 'Spanish' -> 'Hola!'
   // language: 'Chinese' -> 'Ni Hao!'
   // if language is undefined return 'Hello!'
+  if (language === 'German') { return 'Guten Tag!'; }
+  if (language === 'Chinese') { return 'Ni Hao!'; }
+  if (language === 'Spanish') { return 'Hola!'; }
+  return 'Hello!';
 };
 
 const isTenOrFive = (num) => {
   // return true if num is 10 or 5
   // otherwise return false
+  return num === 10 || num === 5;
 };
 
 const isInRange = (num) => {
   // return true if num is less than 50 and greater than 20
+  return num > 20 && num < 50;
 };
 
 const isInteger = (num) => {
@@ -29,6 +39,7 @@ const isInteger = (num) => {
   // -10 -> true
   // otherwise return false
   // hint: you can solve this using Math.floor
+  return num === Math.floor(num);
 };
 
 const fizzBuzz = (num) => {
@@ -36,6 +47,10 @@ const fizzBuzz = (num) => {
   // if num is divisible by 5 return 'buzz'
   // if num is divisible by 3 & 5 return 'fizzbuzz'
   // otherwise return num
+  if (num % 15 === 0) { return 'fizzbuzz'; }
+  if (num % 3 === 0) { return 'fizz'; }
+  if (num % 5 === 0) { return 'buzz'; }
+  return num;
 };
 
 const isPrime = (num) => {
@@ -44,35 +59,54 @@ const isPrime = (num) => {
   // hint: a prime number is only evenly divisible by itself and 1
   // hint2: you can solve this using a for loop
   // note: 0 and 1 are NOT considered prime numbers
+  if (num <= 1) {
+    return false;
+  }
+
+  for (let i = 2; i < num / 2; i++) {
+    if (num % i === 0) {
+      return false;
+    }
+  }
+  return true;
 };
 
 const returnFirst = (arr) => {
   // return the first item from the array
+  return arr[0];
 };
 
 const returnLast = (arr) => {
   // return the last item of the array
+  return arr[arr.length - 1];
 };
 
 const getArrayLength = (arr) => {
   // return the length of the array
+  return arr.length;
 };
 
 const incrementByOne = (arr) => {
   // arr is an array of integers
   // increase each integer by one
   // return the array
+  const addOne = (x) => { return x + 1; };
+  return arr.map(addOne);
 };
 
 const addItemToArray = (arr, item) => {
   // add the item to the end of the array
   // return the array
+  arr.push(item);
+  return arr;
 };
 
 const addItemToFront = (arr, item) => {
   // add the item to the front of the array
   // return the array
   // hint: use the array method .unshift
+  arr.unshift(item);
+  return arr;
 };
 
 const wordsToSentence = (words) => {
@@ -80,26 +114,41 @@ const wordsToSentence = (words) => {
   // return a string that is all of the words concatenated together
   // spaces need to be between each word
   // example: ['Hello', 'world!'] -> 'Hello world!'
+  return words.join(' ');
 };
 
 const contains = (arr, item) => {
   // check to see if item is inside of arr
   // return true if it is, otherwise return false
+  return arr.includes(item);
 };
 
 const addNumbers = (numbers) => {
   // numbers is an array of integers.
   // add all of the integers and return the value
+  let sum = 0;
+  numbers.forEach((num) => { sum += num; });
+  return sum;
 };
 
 const averageTestScore = (testScores) => {
   // testScores is an array.  Iterate over testScores and compute the average.
   // return the average
+  let sum = 0;
+  testScores.forEach((score) => { sum += score; });
+  return sum / testScores.length;
 };
 
 const largestNumber = (numbers) => {
   // numbers is an array of integers
   // return the largest integer
+  let max = numbers[0];
+  numbers.forEach((num) => {
+    if (num > max) {
+      max = num;
+    }
+  });
+  return max;
 };
 
 // Do not modify code below this line.
