9b8b4d953cf136f54dd0a8f2439a7e6686e53eb9
Brandon Fizer <Brandon.m.fizer@gmail.com>
2017-09-05 20:34:43 -0400

    Project 1 & 2

--- src/project-2.js
+++ src/project-2.js
@@ -1,9 +1,6 @@
 // Do not change any of the function names
 
-const getBiggest = (x, y) => {
-  // x and y are integers.  Return the larger integer
-  // if they are the same return either one
-};
+const getBiggest = (x, y) => { return (x >= y) ? x : y; };
 
 const greeting = (language) => {
   // return a greeting for three different languages:
@@ -11,31 +8,45 @@ const greeting = (language) => {
   // language: 'Spanish' -> 'Hola!'
   // language: 'Chinese' -> 'Ni Hao!'
   // if language is undefined return 'Hello!'
+  switch (language) {
+    case 'German' :
+      return 'Guten Tag!';
+    case 'Spanish' :
+      return 'Hola!';
+    case 'Chinese' :
+      return 'Ni Hao!';
+    default :
+      return 'Hello!';
+  }
 };
 
-const isTenOrFive = (num) => {
+const isTenOrFive = num => num === 10 || num === 5;
   // return true if num is 10 or 5
   // otherwise return false
-};
 
-const isInRange = (num) => {
+
+const isInRange = num => num > 20 && num < 50;
   // return true if num is less than 50 and greater than 20
-};
 
-const isInteger = (num) => {
+
+const isInteger = num => Math.floor(num) >= 1 || Math.ceil(num) <= 0;
   // return true if num is an integer
   // 0.8 -> false
   // 1 -> true
   // -10 -> true
   // otherwise return false
   // hint: you can solve this using Math.floor
-};
+
 
 const fizzBuzz = (num) => {
   // if num is divisible by 3 return 'fizz'
   // if num is divisible by 5 return 'buzz'
   // if num is divisible by 3 & 5 return 'fizzbuzz'
   // otherwise return num
+  if ((num % 5 === 0) && (num % 3 === 0)) return 'fizzbuzz';
+  if (num % 5 === 0) return 'buzz';
+  if (num % 3 === 0) return 'fizz';
+  return num;
 };
 
 const isPrime = (num) => {
@@ -44,62 +55,78 @@ const isPrime = (num) => {
   // hint: a prime number is only evenly divisible by itself and 1
   // hint2: you can solve this using a for loop
   // note: 0 and 1 are NOT considered prime numbers
+  for (let i = 2; i < num; i++) {
+    if (num % i === 0) return false;
+  }
+  return num > 1;
 };
 
-const returnFirst = (arr) => {
+const returnFirst = arr => arr[0];
   // return the first item from the array
-};
 
-const returnLast = (arr) => {
+
+const returnLast = arr => arr[arr.length - 1];
   // return the last item of the array
-};
 
-const getArrayLength = (arr) => {
+
+const getArrayLength = arr => arr.length;
   // return the length of the array
-};
 
-const incrementByOne = (arr) => {
+
+const incrementByOne = arr => arr.map(val => val + 1);
   // arr is an array of integers
   // increase each integer by one
   // return the array
-};
+
 
 const addItemToArray = (arr, item) => {
+  arr.push(item);
+  return arr;
+};
   // add the item to the end of the array
   // return the array
-};
+
 
 const addItemToFront = (arr, item) => {
   // add the item to the front of the array
   // return the array
   // hint: use the array method .unshift
+  arr.unshift(item);
+  return arr;
 };
 
-const wordsToSentence = (words) => {
+const wordsToSentence = words => words.join(' ');
   // words is an array of strings
   // return a string that is all of the words concatenated together
   // spaces need to be between each word
   // example: ['Hello', 'world!'] -> 'Hello world!'
-};
+
 
 const contains = (arr, item) => {
   // check to see if item is inside of arr
   // return true if it is, otherwise return false
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === item) return true;
+  }
+  return false;
 };
 
 const addNumbers = (numbers) => {
   // numbers is an array of integers.
   // add all of the integers and return the value
+  return numbers.reduce((val, next) => { return val + next; }, 0);
 };
 
 const averageTestScore = (testScores) => {
   // testScores is an array.  Iterate over testScores and compute the average.
   // return the average
+  return testScores.reduce((val, next) => { return val + next; }, 0) / testScores.length;
 };
 
 const largestNumber = (numbers) => {
   // numbers is an array of integers
   // return the largest integer
+  return numbers.reduce((last, next) => { return (next > last) ? next : last; });
 };
 
 // Do not modify code below this line.
