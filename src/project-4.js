ce6ebc4c026258d4819763532845231d2c5a2c2f
Adam Oates <writingyourcareer@gmail.com>
2017-09-06 22:50:59 +0000

    completed basic-javascript

--- src/project-4.js
+++ src/project-4.js
@@ -1,31 +1,47 @@
 const getFirstItem = (collection, cb) => {
   // invoke the callback function and pass the first item from the collection in as an argument
+  cb(collection[0]);
 };
 
 const getLength = (collection, cb) => {
   // Write a function called getLength that passes the length of the array into the callback
+  cb(collection.length);
 };
 
 const getLastItem = (collection, cb) => {
   // Write a function called getLastItem which passes the getLastItem item of the array into the callback
+  const last = collection.length - 1;
+  cb(collection[last]);
 };
 
 const sumNums = (x, y, cb) => {
   // Write a function called sumNums that adds two numbers and passes the result to the callback
+  const sum = x + y;
+  cb(sum);
 };
 
 const multiplyNums = (x, y, cb) => {
   // Write a function called multiplyNums that multiplies two numbers and passes the result to the callback
+  const product = x * y;
+  cb(product);
 };
 
 const contains = (collection, item, cb) => {
   // Write a function called contains that checks if an item is present inside of the given array.
   // Pass true to the callback if it is, otherwise pass false
+  cb(collection.includes(item));
 };
 
 const removeDuplicates = (collection, cb) => {
   // Write a function called removeDuplicates that removes all duplicate values from the given array.
   // Pass the array to the callback function.  Do not mutate the original array.
+  const itemsChecked = [];
+  collection.forEach((item, index) => {
+    if (itemsChecked.indexOf(item) === -1) {
+      itemsChecked.push(item);
+    }
+  });
+  cb(itemsChecked);
 };
 
 module.exports = {
