87b08b83c06c9a68ec86c64b4cdcbbcb9868b6d6
ubuntu <irishseanvaldivia@gmail.com>
2018-02-17 15:50:06 -0600

    This object can take its method and shove it up its void!

--- src/project-3.js
+++ src/project-3.js
@@ -16,17 +16,21 @@ const addProperty = (object, property) => {
   // add the property to the object with a value of null
   // return the object
   // note: the property name is NOT 'property'.  The name is the value of the argument called property (a string)
+  object[property] = null;
+  return object;
 };
 
 const invokeMethod = (object, method) => {
   // method is a string that contains the name of a method on the object
   // invoke this method
   // nothing needs to be returned
+  object[method]();
 };
 
 const multiplyMysteryNumberByFive = (mysteryNumberObject) => {
   // mysteryNumberObject has a property called mysteryNumber
   // multiply the mysteryNumber property by 5 and return the product
+  
 };
 
 const deleteProperty = (object, property) => {
