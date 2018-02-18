8250fda976b476aa6db62f4e22b1c1f79cee8291
zackhitch <zackhitch@users.noreply.github.com>
2018-02-17 16:18:15 -0800

    started on project 4

--- src/project-4.js
+++ src/project-4.js
@@ -1,31 +1,51 @@
+/* eslint-disable */
+
 const getFirstItem = (collection, cb) => {
   // invoke the callback function and pass the first item from the collection in as an argument
+  cb(collection.shift());
 };
 
 const getLength = (collection, cb) => {
   // Write a function called getLength that passes the length of the array into the callback
+  cb(collection.length);
 };
 
 const getLastItem = (collection, cb) => {
   // Write a function called getLastItem which passes the getLastItem item of the array into the callback
+  cb(collection.pop());
 };
 
 const sumNums = (x, y, cb) => {
   // Write a function called sumNums that adds two numbers and passes the result to the callback
+  cb(x + y);
 };
 
 const multiplyNums = (x, y, cb) => {
   // Write a function called multiplyNums that multiplies two numbers and passes the result to the callback
+  cb(x * y);
 };
 
 const contains = (collection, item, cb) => {
   // Write a function called contains that checks if an item is present inside of the given array.
   // Pass true to the callback if it is, otherwise pass false
+  cb(function(collection, item) {
+    if (collection.length === 0) {
+      return false;
+    }
+    for (let i in collection) {
+      if (item === collection[i]) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+  })
 };
 
 const removeDuplicates = (collection, cb) => {
   // Write a function called removeDuplicates that removes all duplicate values from the given array.
   // Pass the array to the callback function.  Do not mutate the original array.
+  
 };
 
 module.exports = {
