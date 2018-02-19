6f6eb967bd9471d094a61d2de053e142d16752d3
Yan Kozlovskiy <yankozlovskiy@icloud.com>
2018-02-19 14:32:24 -0500

    push passing project files

--- src/project-1.js
+++ src/project-1.js
@@ -1,140 +1,82 @@
 // Do not change any of the funcxtion names
 
-const multiplyByTen = (num) => {
-  // return num after multiplying it by ten
-  // code here
-};
+// return num after multiplying it by ten
+const multiplyByTen = num => num * 10;
 
-const subtractFive = (num) => {
-  // return num after subtracting five
-  // code here
-};
+// return num after subtracting five
+const subtractFive = num => num - 5;
 
-const areSameLength = (str1, str2) => {
-  // return true if the two strings have the same length
-  // otherwise return false
-  // code here
-};
+// return true if the two strings have the same length
+const areSameLength = (str1, str2) => str1.length === str2.length;
 
-const areEqual = (x, y) => {
-  // return true if x and y are the same
-  // otherwise return false
-  // code here
-};
+// return true if x and y are the same otherwise return false
+const areEqual = (x, y) => x === y;
 
-const lessThanNinety = (num) => {
-  // return true if num is less than ninety
-  // otherwise return false
-  // code here
-};
+// return true if num is less than ninety otherwise return false
+const lessThanNinety = num => num < 90;
 
-const greaterThanFifty = (num) => {
-  // return true if num is greater than fifty
-  // otherwise return false
-  // code here
-};
+// return true if num is greater than fifty otherwise retrun false
+const greaterThanFifty = num => num > 50;
 
-const add = (x, y) => {
-  // add x and y together and return the value
-  // code here
-};
+// add x and y together and return the value
+const add = (x, y) => x + y;
 
-const subtract = (x, y) => {
-  // subtract y from x and return the value
-  // code here
-};
+// subtract y from x and return the value
+const subtract = (x, y) => x - y;
 
-const divide = (x, y) => {
-  // divide x by y and return the value
-  // code here
-};
+// divide x by y and return the value
+const divide = (x, y) => x / y;
 
-const multiply = (x, y) => {
-  // multiply x by y and return the value
-  // code here
-};
+// multiply x by y and return the value
+const multiply = (x, y) => x * y;
 
-const getRemainder = (x, y) => {
-  // return the remainder from dividing x by y
-  // code here
-};
+// return the remainder from dividing x by y
+const getRemainder = (x, y) => x % y;
 
-const isEven = (num) => {
-  // return true if num is even
-  // otherwise return false
-  // code here
-};
+// return true if num is even otherwise return false
+const isEven = num => num % 2 === 0;
 
-const isOdd = (num) => {
-  // return true if num is odd
-  // otherwise return false
-  // code here
-};
+// return true if num is odd otherwise return false
+const isOdd = num => num % 2 !== 0;
 
-const square = (num) => {
-  // square num and return the new value
-  // code here
-};
+// square num and return the new value
+const square = num => num ** 2;
 
-const cube = (num) => {
-  // cube num and return the new value
-  // code here
-};
+// cube num and return the new value
+const cube = num => num ** 3;
 
-const raiseToPower = (num, exponent) => {
-  // raise num to whatever power is passed in as exponent
-  // code here
-};
+// raise num to whatever power is passed in as exponent
+const raiseToPower = (num, exponent) => num ** exponent;
 
-const roundNumber = (num) => {
-  // round num and return it
-  // code here
-};
+// round num and return it
+const roundNumber = num => Math.round(num);
 
-const roundUp = (num) => {
-  // round num up and return it
-  // code here
-};
+// round num up and return it
+const roundUp = num => Math.ceil(num);
 
-const addExclamationPoint = (str) => {
-  // add an exclamation point to the end of str and return the new string
-  // 'hello world' -> 'hello world!'
-  // code here
-};
+// add an exclamation point to the end of str and return the new string 'hello world' -> 'hello world!'
+const addExclamationPoint = str => `${str}!`;
 
-const combineNames = (firstName, lastName) => {
-  // return firstName and lastName combined as one string and separated by a space.
-  // 'Lambda', 'School' -> 'Lambda School'
-  // code here
-};
+// return firstName and lastName combined as one string and separated by a space. 'Lambda', 'School' -> 'Lambda School'
+const combineNames = (firstName, lastName) => `${firstName} ${lastName}`;
 
-const getGreeting = (name) => {
-  // Take the name string and concatenate other strings onto it so it takes the following form:
-  // 'Sam' -> 'Hello Sam!'
-  // code here
-};
+// Take the name string and concatenate other strings onto it so it takes the following form: 'Sam' -> 'Hello Sam!'
+const getGreeting = name => `Hello ${name}!`;
 
 // If you can't remember these area formulas then head over to Google or look at the test code.
 
-const getRectangleArea = (length, width) => {
-  // return the area of the rectangle by using length and width
-  // code here
-};
+// return the area of the rectangle by using length and width
+const getRectangleArea = (length, width) => length * width;
 
-const getTriangleArea = (base, height) => {
-  // return the area of the triangle by using base and height
-  // code here
-};
+// return the area of the triangle by using base and height
+const getTriangleArea = (base, height) => (base / 2) * height;
 
-const getCircleArea = (radius) => {
-  // return the rounded area of the circle given the radius
-  // code here
-};
+// return the rounded area of the circle given the radius
+const getCircleArea = radius => Math.floor(Math.PI * Math.pow(radius, 2));
 
-const getRectangularPrismVolume = (length, width, height) => {
-  // return the volume of the 3D rectangular prism given the length, width, and height
-  // code here
-};
+// return the volume of the 3D rectangular prism given the length, width, and height
+const getRectangularPrismVolume = (length, width, height) => length * width * height;
+
 
 // Do not modify code below this line.
 // --------------------------------
--- src/project-3.js
+++ src/project-3.js
@@ -1,94 +1,131 @@
 // Do not change any of the function names
 
-const makeCat = (name, age) => {
-  // create a new object with a name property with the value set to the name argument
-  // add an age property to the object with the value set to the age argument
-  // add a method called meow that returns the string 'Meow!'
-  // return the object
+// create a new object with a name property with the value set to the name argument
+// add an age property to the object with the value set to the age argument
+// add a method called meow that returns the string 'Meow!'
+// return the object
+const makeCat = (catName, catAge) => {
+  const cat = {
+    name: catName,
+    age: catAge,
+    meow: () => 'Meow!'
+  };
+  return cat;
 };
 
+// add the property to the object with a value of null
+// return the object
+// note: the property name is NOT 'property'.  The name is the value of the argument called property (a string)
 const addProperty = (object, property) => {
-  // add the property to the object with a value of null
-  // return the object
-  // note: the property name is NOT 'property'.  The name is the value of the argument called property (a string)
+  object[property] = null;
+  return object;
 };
-
+// method is a string that contains the name of a method on the object
+// invoke this method
+// nothing needs to be returned
 const invokeMethod = (object, method) => {
-  // method is a string that contains the name of a method on the object
-  // invoke this method
-  // nothing needs to be returned
+  object[method]();
 };
 
+// mysteryNumberObject has a property called mysteryNumber
+// multiply the mysteryNumber property by 5 and return the product
 const multiplyMysteryNumberByFive = (mysteryNumberObject) => {
-  // mysteryNumberObject has a property called mysteryNumber
-  // multiply the mysteryNumber property by 5 and return the product
+  return mysteryNumberObject.mysteryNumber * 5;
 };
 
+// remove the property from the object
+// return the object
 const deleteProperty = (object, property) => {
-  // remove the property from the object
-  // return the object
+  delete object[property];
+  return object;
 };
 
-const newUser = (name, email, password) => {
-  // create a new object with properties matching the arguments passed in.
-  // return the new object
+// create a new object with properties matching the arguments passed in.
+// return the new object
+const newUser = (userName, userEmail, userPassword) => {
+  return {
+    name: userName,
+    email: userEmail,
+    password: userPassword
+  };
 };
 
-const hasEmail = (user) => {
-  // return true if the user has a value for the property 'email'
-  // otherwise return false
-};
+// return true if the user has a value for the property 'email'
+// otherwise return false
+const hasEmail = user => !!user.email;
 
+
+// return true if the object has the value of the property argument
+// property is a string
+// otherwise return false
 const hasProperty = (object, property) => {
-  // return true if the object has the value of the property argument
-  // property is a string
-  // otherwise return false
+  return !!object[property];
 };
 
+// check to see if the provided password matches the password property on the user object
+// return true if they match
+// otherwise return false
 const verifyPassword = (user, password) => {
-  // check to see if the provided password matches the password property on the user object
-  // return true if they match
-  // otherwise return false
+  return user.password === password;
 };
 
+// replace the existing password on the user object with the value of newPassword
+// return the object
 const updatePassword = (user, newPassword) => {
-  // replace the existing password on the user object with the value of newPassword
-  // return the object
+  user.password = newPassword;
+  return user;
 };
 
+// user has a property called friends that is an array
+// add newFriend to the end of the friends array
+// return the user object
 const addFriend = (user, newFriend) => {
-  // user has a property called friends that is an array
-  // add newFriend to the end of the friends array
-  // return the user object
+  user.friends.push(newFriend);
+  return user;
 };
 
+// users is an array of user objects.
+// each user object has the property 'isPremium'
+// set each user's isPremium property to true
+// return the users array
 const setUsersToPremium = (users) => {
-  // users is an array of user objects.
-  // each user object has the property 'isPremium'
-  // set each user's isPremium property to true
-  // return the users array
+  for (let i = 0; i < users.length; i++) {
+    users[i].isPremium = true;
+  }
+  return users;
 };
 
+// user has an array property called 'posts'
+// posts is an array of post objects
+// each post object has an integer property called 'likes'
+// sum together the likes from all the post objects
+// return the sum
 const sumUserPostLikes = (user) => {
-  // user has an array property called 'posts'
-  // posts is an array of post objects
-  // each post object has an integer property called 'likes'
-  // sum together the likes from all the post objects
-  // return the sum
+  let sumLikes = 0;
+  for (let i = 0; i < user.posts.length; i++) {
+    sumLikes += user.posts[i].likes;
+  }
+  return sumLikes;
 };
 
+// Add a method to the storeItem object called 'calculateDiscountPrice'
+// This method should multiply the storeItem's 'price' and 'discountPercentage' to get the discount
+// The method then subtracts the discount from the price and returns the discounted price
+// example:
+// price -> 20
+// discountPercentage -> .2
+// discountPrice = 20 - (20 * .2)
+// Make sure you return storeItem after adding the method to it
+// hint: arrow functions don't bind a this
 const addCalculateDiscountPriceMethod = (storeItem) => {
-  // Add a method to the storeItem object called 'calculateDiscountPrice'
-  // This method should multiply the storeItem's 'price' and 'discountPercentage' to get the discount
-  // The method then subtracts the discount from the price and returns the discounted price
-  // example:
-  // price -> 20
-  // discountPercentage -> .2
-  // discountPrice = 20 - (20 * .2)
-  // Make sure you return storeItem after adding the method to it
-  // hint: arrow functions don't bind a this
+  const priceWithDiscount = () => {
+    const discountAmount = storeItem.price * storeItem.discountPercentage;
+    const discount = storeItem.price - discountAmount;
+    return discount;
+  };
+  storeItem.calculateDiscountPrice = priceWithDiscount;
+  return storeItem;
 };
-
 // Do not modify code below this line.
 // --------------------------------
 
