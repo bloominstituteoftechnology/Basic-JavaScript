8fc3e2ce3c349b5729e958a10418374ee8c4f808
Brendan <bmeredith@openmailbox.org>
2018-02-19 13:35:08 -0600

    Completed lessons 1 and 2

--- src/project-2.js
+++ src/project-2.js
@@ -3,6 +3,7 @@
 const getBiggest = (x, y) => {
   // x and y are integers.  Return the larger integer
   // if they are the same return either one
+  return Math.max(x, y);
 };
 
 const greeting = (language) => {
@@ -10,16 +11,25 @@ const greeting = (language) => {
   // language: 'German' -> 'Guten Tag!'
   // language: 'Spanish' -> 'Hola!'
   // language: 'Chinese' -> 'Ni Hao!'
-  // if language is undefined return 'Hello!'
+  // if language is undefined
+  if (language === 'German') {
+    return 'Guten Tag!';
+  } else if (language === 'Spanish') {
+    return 'Hola!';
+  } else if (language === 'Chinese') {
+    return 'Ni Hao!';
+  } return 'Hello!';
 };
 
 const isTenOrFive = (num) => {
   // return true if num is 10 or 5
   // otherwise return false
+  return num === 10 || num === 5;
 };
 
 const isInRange = (num) => {
   // return true if num is less than 50 and greater than 20
+  return num < 50 && num > 20;
 };
 
 const isInteger = (num) => {
@@ -29,6 +39,7 @@ const isInteger = (num) => {
   // -10 -> true
   // otherwise return false
   // hint: you can solve this using Math.floor
+  return num === Math.floor(num);
 };
 
 const fizzBuzz = (num) => {
@@ -36,6 +47,13 @@ const fizzBuzz = (num) => {
   // if num is divisible by 5 return 'buzz'
   // if num is divisible by 3 & 5 return 'fizzbuzz'
   // otherwise return num
+  if (num % 3 === 0 && num % 5 === 0) {
+    return 'fizzbuzz';
+  } else if (num % 3 === 0) {
+    return 'fizz';
+  } else if (num % 5 === 0) {
+    return 'buzz';
+  } return num;
 };
 
 const isPrime = (num) => {
@@ -44,35 +62,52 @@ const isPrime = (num) => {
   // hint: a prime number is only evenly divisible by itself and 1
   // hint2: you can solve this using a for loop
   // note: 0 and 1 are NOT considered prime numbers
+  for (let i = 2; i < num; i++) {
+    if (num % i === 0) {
+      return false;
+    }
+  }
+  return num > 1;
 };
 
 const returnFirst = (arr) => {
   // return the first item from the array
+  return arr[0];
 };
 
 const returnLast = (arr) => {
   // return the last item of the array
+  return arr[arr.length - 1];
 };
 
 const getArrayLength = (arr) => {
   // return the length of the array
+  return arr.length;
 };
 
 const incrementByOne = (arr) => {
   // arr is an array of integers
   // increase each integer by one
   // return the array
+  for (let i = 0; i < arr.length; i++) {
+    arr[i] += 1;
+  }
+  return arr;
 };
 
 const addItemToArray = (arr, item) => {
   // add the item to the end of the array
   // return the array
+  arr.push(item);
+  return arr;
 };
 
 const addItemToFront = (arr, item) => {
   // add the item to the front of the array
   // return the array
   // hint: use the array method .unshift
+  arr.unshift(item);
+  return arr;
 };
 
 const wordsToSentence = (words) => {
@@ -80,26 +115,39 @@ const wordsToSentence = (words) => {
   // return a string that is all of the words concatenated together
   // spaces need to be between each word
   // example: ['Hello', 'world!'] -> 'Hello world!'
+  return words.join(' ');
 };
 
 const contains = (arr, item) => {
   // check to see if item is inside of arr
   // return true if it is, otherwise return false
+  return arr.includes(item);
 };
 
 const addNumbers = (numbers) => {
   // numbers is an array of integers.
   // add all of the integers and return the value
+  let value = 0;
+  for (let i = 0; i < numbers.length; i++) {
+    value += numbers[i];
+  }
+  return value;
 };
 
 const averageTestScore = (testScores) => {
   // testScores is an array.  Iterate over testScores and compute the average.
   // return the average
+  let value = 0;
+  for (let i = 0; i < testScores.length; i++) {
+    value += testScores[i];
+  }
+  return value / testScores.length;
 };
 
 const largestNumber = (numbers) => {
   // numbers is an array of integers
   // return the largest integer
+  return Math.max(...numbers);
 };
 
 // Do not modify code below this line.
