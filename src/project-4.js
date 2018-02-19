ee64449a574e96b94b88fd2798ee388edb7d8874
Tom Utsey <tom@edgegluer.com>
2018-02-19 10:01:02 -0500

    Thomas Utsey JS practice 1 - 4 completed.

--- src/project-4.js
+++ src/project-4.js
@@ -1,31 +1,50 @@
 const getFirstItem = (collection, cb) => {
   // invoke the callback function and pass the first item from the collection in as an argument
+  return cb(collection[0]);
 };
 
 const getLength = (collection, cb) => {
   // Write a function called getLength that passes the length of the array into the callback
+  cb(collection.length);
 };
 
 const getLastItem = (collection, cb) => {
   // Write a function called getLastItem which passes the getLastItem item of the array into the callback
+  cb(collection.pop());
 };
 
 const sumNums = (x, y, cb) => {
   // Write a function called sumNums that adds two numbers and passes the result to the callback
+  cb(x + y);
 };
 
 const multiplyNums = (x, y, cb) => {
   // Write a function called multiplyNums that multiplies two numbers and passes the result to the callback
+  cb(x * y);
 };
 
 const contains = (collection, item, cb) => {
   // Write a function called contains that checks if an item is present inside of the given array.
   // Pass true to the callback if it is, otherwise pass false
+  for (let i = 0; i < collection.length; i++) {
+    if (collection[i] === item) {
+      cb(true);
+    } else {
+      cb(false);
+    }
+  }
 };
 
 const removeDuplicates = (collection, cb) => {
   // Write a function called removeDuplicates that removes all duplicate values from the given array.
   // Pass the array to the callback function.  Do not mutate the original array.
+  const sarr = collection.slice().sort();
+  for (let i = 0; i < sarr.length - 1; i++) {
+    if (sarr[i + 1] === sarr[i]) {
+      sarr.pop([i]);
+    }
+  }
+  return cb(sarr);
 };
 
 module.exports = {
