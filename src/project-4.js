f2aea21b577737a0bdf2049151945a28ed505ab2
Brendan <bmeredith@openmailbox.org>
2018-02-19 21:39:17 -0600

    1,2,4 Complete 3 Close

--- src/project-4.js
+++ src/project-4.js
@@ -32,6 +32,7 @@ const contains = (collection, item, cb) => {
 const removeDuplicates = (collection, cb) => {
   // Write a function called removeDuplicates that removes all duplicate values from the given array.
   // Pass the array to the callback function.  Do not mutate the original array.
+  return cb(Array.from(new Set(collection)));
 };
 
 module.exports = {
