cd32fd8561ef4806735146575044041db8d7770d
Iqra Javed <iqrajaved@Iqras-MacBook-Air.local>
2018-02-19 22:45:37 -0600

    completed project 2 and 3

--- src/project-4.js
+++ src/project-4.js
@@ -1,31 +1,47 @@
 const getFirstItem = (collection, cb) => {
   // invoke the callback function and pass the first item from the collection in as an argument
+  cb(collection[0]);
 };
 
 const getLength = (collection, cb) => {
   // Write a function called getLength that passes the length of the array into the callback
+  cb(collection.length);
 };
 
 const getLastItem = (collection, cb) => {
   // Write a function called getLastItem which passes the getLastItem item of the array into the callback
+  cb(collection[collection.length - 1]);
 };
 
 const sumNums = (x, y, cb) => {
   // Write a function called sumNums that adds two numbers and passes the result to the callback
+  const sum = x + y;
+  cb(sum);
 };
 
 const multiplyNums = (x, y, cb) => {
   // Write a function called multiplyNums that multiplies two numbers and passes the result to the callback
+  const product = x * y;
+  cb(x * y);
 };
 
 const contains = (collection, item, cb) => {
   // Write a function called contains that checks if an item is present inside of the given array.
   // Pass true to the callback if it is, otherwise pass false
+  if (collection.includes(item)) {
+    cb(true);
+    return;
+  }
+  cb(false);
 };
 
 const removeDuplicates = (collection, cb) => {
   // Write a function called removeDuplicates that removes all duplicate values from the given array.
   // Pass the array to the callback function.  Do not mutate the original array.
+  const hasDuplicate = (item, index) => {
+    return !(collection.slice(index + 1).includes(item));
+  };
+  cb(collection.filter(hasDuplicate));
 };
 
 module.exports = {
